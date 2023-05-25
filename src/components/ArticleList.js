09cc4f1d972449f61118e7e33844166e7841dc2e
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-05-24 19:44:40 -0700

    complete mini project

---
+++ src/components/ArticleList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}){
+    
+     const articallistObj = posts.map((artical)=>{
+         return <Article key={artical.id} title={artical.title} date={artical.date} preview={artical.preview}/>
+     })
+    return (
+        <main>
+            {articallistObj} 
+        </main>
+    )
+}
+export default ArticleList;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(nameobj){
+    return (
+        <header>
+            <h1>{nameobj.name}</h1>
+        </header>
+    )
+}
+export default Header;
